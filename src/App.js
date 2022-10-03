6349404fb952c85a85765ebeafcd3741b687b883
erlindiisaj <erlind.isaj02@gmail.com>
2022-10-03 18:07:54 +0200

    Header added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/header/header.component';
+
 import HomePage from './pages/homepage/homepage.component';
 
 import ShopPage from './pages/shop/shop.component';
@@ -9,6 +11,7 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route exact path='/shop' element={<ShopPage />} />
